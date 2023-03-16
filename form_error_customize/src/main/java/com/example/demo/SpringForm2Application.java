0990579ea7f5f235e0b55b364caa5e462434ad12
Miho_S <milfy6210choco@gmail.com>
2023-03-16 19:47:21 +0900

    -stduy_of_springboot-

---
+++ form_error_customize/src/main/java/com/example/demo/SpringForm2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForm2Application.class, args);
+	}
+
+}
