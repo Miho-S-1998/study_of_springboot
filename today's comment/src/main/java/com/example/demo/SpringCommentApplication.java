2f88128f15d05932865df6a48064500736bdf3a7
Miho_S <milfy6210choco@gmail.com>
2023-03-18 20:48:15 +0900

    -Log_of_SpringBoot-

---
+++ today's comment/src/main/java/com/example/demo/SpringCommentApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommentApplication.class, args);
+	}
+
+}
