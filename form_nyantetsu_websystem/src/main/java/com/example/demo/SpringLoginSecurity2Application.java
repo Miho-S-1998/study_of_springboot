27dc921494c19da27dc8279211a5fae2a4019dab
Miho_S <milfy6210choco@gmail.com>
2023-03-28 23:03:40 +0900

    -Log_of_SpringBoot-

---
+++ form_nyantetsu_websystem/src/main/java/com/example/demo/SpringLoginSecurity2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginSecurity2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginSecurity2Application.class, args);
+	}
+
+}
